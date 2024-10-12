4904434f62e393d9db1007b15b88bb4ae0c20418
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-10-11 22:54:33 -0700

    comments-navbar

--- src/app/(header)/navbar/page.tsx
+++ src/app/(header)/navbar/page.tsx
@@ -3,26 +3,43 @@ import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <div><header className="text-gray-600 body-font bg-indigo-500">
-            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-                        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                    </svg>
-                    <span className="ml-3 text-xl"><Link className="mr-5 hover:text-gray-100" href="/">MY PORTFOLIO</Link></span>
-                </a>
-                <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-                    <Link className="mr-5 hover:text-gray-100" href="/">Home</Link>
-                    <Link className="mr-5 hover:text-gray-100" href="/about">About</Link>
-                    <Link className="mr-5 hover:text-gray-100" href="/services">Services</Link>
-                    <Link className="mr-5 hover:text-gray-100" href="/contact">Contact</Link>
-                </nav>
-                <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Login
-                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
-                        <path d="M5 12h14M12 5l7 7-7 7"></path>
-                    </svg>
-                </button>
-            </div>
-        </header></div>
+        <div>
+
+            {/* Header  */}
+
+            <header className="text-gray-600 body-font bg-indigo-500">
+                <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                    <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+                            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+                        </svg>
+
+                        {/* Website Name */}
+
+                        <span className="ml-3 text-xl"><Link className="mr-5 hover:text-gray-100" href="/">MY PORTFOLIO</Link></span>
+                    </a>
+
+                    {/* Navbar */}
+
+                    <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+                        <ul className="flex">
+                            <li><Link className="mr-5 hover:text-gray-100" href="/">Home</Link></li>
+                            <li><Link className="mr-5 hover:text-gray-100" href="/about">About</Link></li>
+                            <li><Link className="mr-5 hover:text-gray-100" href="/services">Services</Link></li>
+                            <li><Link className="mr-5 hover:text-gray-100" href="/contact">Contact</Link></li>
+                        </ul>
+                    </nav>
+
+
+                    {/* navbar Butoon right side */}
+
+                    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Login
+                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
+                            <path d="M5 12h14M12 5l7 7-7 7"></path>
+                        </svg>
+                    </button>
+                </div>
+            </header>
+        </div>
     );
 }
