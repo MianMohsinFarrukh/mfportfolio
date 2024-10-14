2f5e5d99a41c9eab4ab130d2e37e3a4e637d385f
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-10-13 23:24:39 -0700

    commnt

--- src/app/(header)/navbar/page.tsx
+++ src/app/(header)/navbar/page.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <div>
 
-            {/* Header  */}
+            {/* HEADER  */}
 
             <header className="text-gray-600 body-font bg-indigo-500">
                 <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
@@ -14,12 +14,12 @@ export default function Navbar() {
                             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
                         </svg>
 
-                        {/* Website Name */}
+                        {/* WEBSITE NAME */}
 
                         <span className="ml-3 text-xl"><Link className="mr-5 hover:text-gray-100" href="/">MY PORTFOLIO</Link></span>
                     </a>
 
-                    {/* Navbar */}
+                    {/* NAVBAR */}
 
                     <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
                         <ul className="flex">
@@ -31,7 +31,7 @@ export default function Navbar() {
                     </nav>
 
 
-                    {/* navbar Butoon right side */}
+                    {/* NAVBAR BUTTON RIGHT SIDE */}
 
                     <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Login
                         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
